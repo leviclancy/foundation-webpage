64064036bd151bfb00208a8dea5f6262bb4b26eb
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2019-12-23 22:00:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?
 
-// Home ... Current events, testimonials, 
-
-// About ... Mission statement
+// Home ... Mission statement
 — The Foundation of Ours is devoted to meeting the material and spiritual needs of Jewish expression in the Kurdistan Region and Iraq, and providing platforms for reconciliation and coexistence with our surrounding communities.
 
+// Calendar
+// We will have a welcoming, multifaith prayer.
+  
 // Museum of Ours
 Museum of Ours works in Erbil and the broader Kurdistan Region, Nineveh Plains, and federal Iraq. We strive to set new standards in communicating the history of our region from ancient times to the present day. Our approaches are research-based and compelling, in order for our audiences to better understand this region's diverse communities including their own. This empowers intellectualization, awareness, and inclusion, as well as disempowers outrage, misinformation, and exclusion.
 Research, events, acquisitions, content,
