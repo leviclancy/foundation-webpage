afe7ff594f0164e1e8a78a608285a57dad775363
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2019-12-23 21:57:24 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<?
+
+// Home ... Current events, testimonials, 
+
+// About ... Mission statement
+— The Foundation of Ours is devoted to meeting the material and spiritual needs of Jewish expression in the Kurdistan Region and Iraq, and providing platforms for reconciliation and coexistence with our surrounding communities.
+
+// Museum of Ours
+Museum of Ours works in Erbil and the broader Kurdistan Region, Nineveh Plains, and federal Iraq. We strive to set new standards in communicating the history of our region from ancient times to the present day. Our approaches are research-based and compelling, in order for our audiences to better understand this region's diverse communities including their own. This empowers intellectualization, awareness, and inclusion, as well as disempowers outrage, misinformation, and exclusion.
+Research, events, acquisitions, content,
+
+// Aron Kodesh
+A space that can meet the needs of a synagogue.
+
+// Kosher Café and Guesthouse
+Café, market, and guesthouse.
+
+// Microsites
+// Check out our digital experiences on specific topics.
+// ... ours.foundation/fire, ours.foundation/lalish, ours.foundation/judaism/, ours.foundation/christianity/, ours.foundation/islam
+
+// Podcast ... Coming soon!
+
+// Press releases ... ours.foundation/press-releases
+
+// Database ... database.ours.foundation
+
+// Footer
+
+// Facebook
+// +1-424-248-7101
+// info@ours.foundation
+
+?>
