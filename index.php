f7501b4e2c7ad937c839e0c0a6b9f359a8918e29
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-01-26 16:21:26 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,57 @@
-<?
+<h1>Foundation of Ours</h1>
 
-// Home ... Mission statement
-— Foundation of Ours is devoted to meeting the material and spiritual needs of Jewish expression in the Kurdistan Region and Iraq, and providing platforms for reconciliation and coexistence with our surrounding communities.
+<div id='foundation-section'>>
 
-// Calendar
-// We will have a welcoming, multifaith prayer.
-  
-// Museum of Ours
-Museum of Ours works in Erbil and the broader Kurdistan Region, Nineveh Plains, and federal Iraq. We strive to set new standards in communicating the history of our region from ancient times to the present day. Our approaches are research-based and compelling, in order for our audiences to better understand this region's diverse communities including their own. This empowers intellectualization, awareness, and inclusion, as well as disempowers outrage, misinformation, and exclusion.
-Research, events, acquisitions, content,
+<h2>Mission</h2>
 
-// Aron Kodesh
-A space that can meet the needs of a synagogue.
+<p>Foundation of Ours is devoted to meeting the material and spiritual needs of Jewish expression in the Kurdistan Region and Iraq, and providing platforms for reconciliation and coexistence with non-Jewish communities.</p>
 
-// Kosher Café and Guesthouse
-Café, market, and guesthouse.
+<h2>Vision</h2>
 
-// Microsites
-// Check out our digital experiences on specific topics.
-// ... ours.foundation/fire, ours.foundation/lalish, ours.foundation/judaism/, ours.foundation/christianity/, ours.foundation/islam
+<p>We envision a more cohesive society in the Kurdistan Region and Iraq, with better awareness of combined and reconciliatory historical narratives and greater tolerance for minorities including a Jewish presence.</p>
 
-// Podcast ... Coming soon!
+<h2>Goals</h2>
 
-// Press releases ... ours.foundation/press-releases
+<ol><li>Establish (then maintain) the <b>Museum of Ours</b>.</li>
+<li>Establish (then maintain) a <b>home for the Torah</b>.</li>
+<li>Provide <b>kosher living</b> including a kitchen, café, lodging, and logistics.</li></ol>
 
-// Database ... database.ours.foundation
+</div>
 
-// Footer
+<div id='museum-section'>
 
-// Facebook
-// +1-424-248-7101
-// info@ours.foundation
+<h2>Museum of Ours</h2>
 
-?>
+<p>Museum of Ours works in Erbil and the broader Kurdistan Region, Nineveh Plains, and federal Iraq. We strive to set new standards in communicating the history of our region from ancient times to the present day. Our approaches are research-based and compelling, in order for our audiences to better understand this region's diverse communities including their own. This empowers intellectualization, awareness, and inclusion, as well as disempowers outrage, misinformation, and exclusion.</p>
+
+<h2>Calendar</h2>
+	
+<ul>
+<li>Combined History of Fire</li>
+<li>Islam</li>
+<li>Christianity</li>
+<li>Judaism</li>
+<li>Judaism</li>
+<li>Judaism</li>
+<li>Combined History of Erbil</li>
+<li>Combined History of Erbil</li>
+<li>Walkig Tour of Erbil</li>
+	</ul>
+
+</div>
+
+<div id='database-section'>
+
+<h2>Database</h2>
+
+<p>Check it out at <a href='https://database.ours.foundation'>database.ours.foundation</a>.</p>
+
+</div>
+
+<div id='press-section'>
+	
+<h2>Contact</h2>
+	
+<p>To contact us, please reach out at +1-424-248-7101 or at hi@ours.foundation</p>
+
+</div>
