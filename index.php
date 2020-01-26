92b6f49b35b54da72b8c00c4456272016cc04979
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-01-26 16:59:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,34 @@
 <link rel="canonical" href="https://ours.foundation/">
 <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
 <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+
+	body {
+		font-family: 'Times New Roman';
+		font-size: 15px;
+		line-height: 28px;
+		}
+	
+	h1, h2, h3, p	{
+		display: block;
+		max-width: 800px;
+		margin: 30px auto;
+		text-align: left;
+		}
+	
+	h1 {
+		font-size: 200%;
+		}
+	
+	h2 {
+		font-size: 160%;
+		}
+
+	h3 {
+		font-size: 130%;
+		}
+
+	
+	</style>	
 </head>
 <body>
 
