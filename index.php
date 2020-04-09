573c01dfd3531a2dac40b2da8c292f15c59b0448
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-09 11:27:52 -0700

    Update index.php

--- index.php
+++ index.php
@@ -119,7 +119,7 @@
 		}
 
 	#section-objectives {
-		background: linear-gradient(165deg, rgba(210,150,90,.7), rgba(170,170,170,.5) 70%), linear-gradient(225deg, rgba(240,150,40,1), rgba(130,130,130,1) 100%);
+		background: linear-gradient(165deg, rgba(210,150,90,.7), rgba(170,170,170,.5) 70%), linear-gradient(315deg, rgba(240,150,40,1), rgba(130,130,130,1) 100%);
 		color: rgba(255,255,255,1);
 		display: block;
 		width: 100%;
