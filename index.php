e1c2e421150a7e5f7f432e7aa5f162e20ae36739
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-06-10 20:53:08 -0700

    Update index.php

--- index.php
+++ index.php
@@ -359,7 +359,7 @@ $css_temp = [
 
 	<h2>Museum of Ours</h2>
 
-	<p><i>Mission statement</i> — Museum of Ours provides exhibitions, discussions, and publications in the Kurdistan Region, Nineveh Plains, and federal Iraq through historiographic, museological and demographic approaches that counter misinformation, systemic biases, and addictions to hate.</p>
+	<p><i>Mission statement</i> — Museum of Ours provides exhibitions, discussions, and publications in the Kurdistan Region, Nineveh Plains, and federal Iraq through historiography, museology, and demographic approaches that counter misinformation, systemic biases, and addictions to hate.</p>
 
 	<a href='https://chat.whatsapp.com/BrRCPuMcj1tCqVZE0ZS2qM' target='_blank'><span>WhatsApp</span></a>
 	<a href='https://www.facebook.com/museum.of.ours/' target='_blank'><span>Facebook</span></a>
